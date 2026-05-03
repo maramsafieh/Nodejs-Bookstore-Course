ad49633cbeda72bfac520c2d5e77690aa776efec
Maram Safieh <maram.s.23.4.2002@gmail.com>
2026-05-03 12:15:42 +0300

    commit

---
+++ data.js
@@ -0,0 +1,48 @@
+const books = [
+    {
+      title: "Black Swan",
+      author: "642d6c525c341f711204f1f2", // مثال لمعرف مؤلف، تأكدي من وضع معرف حقيقي من عندك لاحقاً
+      description: "About Black Swan",
+      price: 10,
+      cover: "soft cover",
+    },
+    {
+      title: "Skin In The Game",
+      author: "642d6c525c341f711204f1f2",
+      description: "About Skin In The Game",
+      price: 12,
+      cover: "soft cover",
+    },
+    {
+      title: "Fooled By Randomness",
+      author: "642d6c525c341f711204f1f2",
+      description: "About Fooled By Randomness",
+      price: 8,
+      cover: "hard cover",
+    },
+    {
+      title: "The Forty Rules Of Love",
+      author: "642d6c525c341f711204f1f2",
+      description: "About The Forty Rules Of Love",
+      price: 14,
+      cover: "soft cover",
+    },
+  ];
+  
+  const authors = [
+    {
+      firstName: "Nassim",
+      lastName: "Taleb",
+      nationality: "Lebanon",
+    },
+    {
+      firstName: "Elif",
+      lastName: "Shafak",
+      nationality: "Turkey",
+    },
+  ];
+  
+  module.exports = {
+    books,
+    authors
+  };
